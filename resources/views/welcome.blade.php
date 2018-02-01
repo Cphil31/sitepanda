7286f2fae86fd91421d9a5de4df5098977d72c82
Cphil31 <cecilephilippe31@gmail.com>
2018-02-01 11:52:16 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    
+@endsection
+
+@section('content')
+    <p>Bienvenue sur mon site dédié aux pandas </p>
+@endsection
