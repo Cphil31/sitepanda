895f4dc457e50490efdfefe94373dba2ab9b5971
Cphil31 <cecilephilippe31@gmail.com>
2018-02-03 11:11:20 +0100

    mise en page

--- resources/views/diaporama.blade.php
+++ resources/views/diaporama.blade.php
@@ -14,7 +14,7 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="http://via.placeholder.com/350x100" alt="First slide">
+      <img class="d-block w-100" src="../resources/pictures/mammal-3074618__340.jpg/350/100" alt="First slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="http://via.placeholder.com/350x100" alt="Second slide">
