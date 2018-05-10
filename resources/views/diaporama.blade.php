d837c9162d4c58417516b928f3bfe551f27287e1
Cphil31 <cecilephilippe31@gmail.com>
2018-05-10 14:48:31 +0200

    images mises sur le carrroussel

--- resources/views/diaporama.blade.php
+++ resources/views/diaporama.blade.php
@@ -14,23 +14,21 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2018/01/10/19/34/mammal-3074618_960_720.jpg" alt="First slide">
+      <img class="d-block w-100" src="https://cdn.rawgit.com/Cphil31/sitepanda/89ee0359/resources/Pictures/adorable-animal-close-up-148182.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/12/26/16/31/panda-3040830_960_720.jpg" alt="Second slide">
+      <img class="d-block w-100" src="https://cdn.rawgit.com/Cphil31/sitepanda/89ee0359/resources/Pictures/animal-branch-cute-145910.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2015/02/26/06/09/panda-649938_960_720.jpg" alt="Third slide">
+      <img class="d-block w-100" src="https://cdn.rawgit.com/Cphil31/sitepanda/89ee0359/resources/Pictures/animal-cute-doll-815491.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/08/10/20/20/pandas-2627426_960_720.jpg" alt="Third slide">
+      <img class="d-block w-100" src="https://cdn.rawgit.com/Cphil31/sitepanda/89ee0359/resources/Pictures/animals-bear-chicken-59720.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/12/27/04/47/panda-3041981_960_720.jpg" alt="Third slide">
+      <img class="d-block w-100" src="https://cdn.rawgit.com/Cphil31/sitepanda/89ee0359/resources/Pictures/breakfast-cafe-caffeine-832827.jpg" alt="Third slide">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2016/10/03/07/00/black-and-white-1711001_960_720.jpg" alt="Third slide">
-    </div>
+    
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
