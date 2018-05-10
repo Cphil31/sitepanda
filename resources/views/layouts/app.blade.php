7b30a8cfe3d6b9013f47bd10655dab451006e2a5
Cphil31 <cecilephilippe31@gmail.com>
2018-05-10 15:51:27 +0200

    mise en page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,10 @@
 <head>
     <title>Site Panda - @yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><link rel="stylesheet"  type="text/css" href="../css/home.css">
+    <link rel="stylesheet"  type="text/css" href="../css/home.css">
     <link rel="stylesheet"  type="text/css" href="../css/qui.css">
     <link rel="stylesheet"  type="text/css" href="../css/diapo.css">
-    <link rel="stylesheet"  type="text/css" href="../css/diapo.css">
+    <link rel="stylesheet"  type="text/css" href="../css/contact.css">
 </head>
 <body>
     @section('sidebar')
