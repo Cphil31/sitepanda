230e2b6c05e2f076d1a96c31ff0a6fbb7b712958
Cphil31 <cecilephilippe31@gmail.com>
2018-05-10 16:10:00 +0200

    mise en page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,4 +10,5 @@
 
 @section('content')
     <div class="bienvenue"> Bienvenue sur mon site dédié aux pandas </div>
+    <div class="question">Mais <a href="/qui" style="color: black">qui</a> sont-ils ?</div>
 @endsection
